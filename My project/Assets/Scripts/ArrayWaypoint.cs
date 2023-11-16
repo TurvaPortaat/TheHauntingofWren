6d8cbd7842e00e71a076752d332bbaad63251524
lattuemmi <emsv97@gmail.com>
2023-11-16 17:34:24 +0200

    Asukkaan korjailua

--- My project/Assets/Scripts/ArrayWaypoint.cs
+++ My project/Assets/Scripts/ArrayWaypoint.cs
@@ -1,44 +1,53 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class AsukasLiikkuu : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    [SerializeField] LayerMask groundLayer;
-
-    //patrol
-    Vector3 destPoint;
-    bool walkpointSet;
-    [SerializeField] float range;
+    // Waypoints
+    public Transform[] waypoints;
+    int currentWaypointIndex = 0;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // Aseta ensimmäinen reittipiste hahmon kohteeksi
+        SetNextWaypoint();
     }
 
     void Update()
     {
-        Patrol();
-    }
+        Debug.Log("Distance to waypoint: " + Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position));
 
-    void Patrol()
-    {
-        if (!walkpointSet) SearchForDest();
-        if (walkpointSet) agent.SetDestination(destPoint);
-        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
-    }
-
-    void SearchForDest()
-    {
-        float z = Random.Range(-range, range);
-        float x = Random.Range(-range, range);
-
-        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
-
-        if(Physics.Raycast(destPoint, Vector3.down, groundLayer))
+        // Tarkista etäisyys ja käytä myös NavMeshAgentin remainingDistance-tarkistusta
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.5f ||
+            agent.remainingDistance < 0.1f)
         {
-            walkpointSet = true;
+            SetNextWaypoint();
         }
+    }
+
+
+    void SetNextWaypoint()
+    {
+        Debug.Log("Setting next waypoint");
+
+        // Tarkista, onko taulukossa enemmän reittipisteitä
+        if (currentWaypointIndex < waypoints.Length - 1)
+        {
+            // Jos on, siirry seuraavaan reittipisteeseen
+            currentWaypointIndex++;
+        }
+        else
+        {
+            // Jos ei, palaa ensimmäiseen reittipisteeseen
+            currentWaypointIndex = 0;
+        }
+
+        // Aseta hahmon kohteeksi uusi reittipiste
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
     }
 }
