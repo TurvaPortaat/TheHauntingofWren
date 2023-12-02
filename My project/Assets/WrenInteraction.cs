d46ef1d30f5b7e7225473a6e2df5122496bbbf93
KirsiL <117641781+TurvaPortaat@users.noreply.github.com>
2023-12-02 15:26:12 +0200

    Interaction WreniPianoJEij
    Marian Jeesillä tehtii Interaction logiikkaa

---
+++ My project/Assets/WrenInteraction.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrenInteraction : MonoBehaviour
+{
+    public AudioSource Piano;   //Tarkista että oikea käytössä, vink unityforum
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Interactable")
+        {
+            Debug.Log("Wren huomasi Esineen");
+
+            if (other.gameObject.name == "pianoWren")
+            {
+                Debug.Log("Piano tos!");
+                //PlayAudio diipadaapa
+            }
+            /* 
+             * if(other.gameObject.name == "Television")
+             * {
+             *      Debug.Log("Telkkari tos!");
+             * }
+             */
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
