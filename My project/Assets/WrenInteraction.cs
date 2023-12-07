9786cffabb2e6bf35732b35c7771a947f5c4ab1e
KirsiL <117641781+TurvaPortaat@users.noreply.github.com>
2023-12-07 12:46:28 +0200

    interact
    Interactattavat esineet määritelty, koodissa wren tunnistaa esineen, ja kertoo mikä siinä on.

--- My project/Assets/WrenInteraction.cs
+++ My project/Assets/WrenInteraction.cs
@@ -17,12 +17,52 @@ public class WrenInteraction : MonoBehaviour
                 Debug.Log("Piano tos!");
                 //PlayAudio diipadaapa
             }
-            /* 
-             * if(other.gameObject.name == "Television")
-             * {
-             *      Debug.Log("Telkkari tos!");
-             * }
-             */
+             
+            if(other.gameObject.name == "Television")
+            {
+                Debug.Log("Telkkari tos!");
+               //trigger event -> animation and sound
+            }
+
+            if(other.gameObject.name == "fridgeWren")
+            {
+               Debug.Log("J‰‰kaappi tos!");
+            }
+
+            if (other.gameObject.name == "hellaWren")
+            {
+                Debug.Log("Hella tos!");
+            }
+
+            if (other.gameObject.name == "ekaKaappiWren")
+            {
+                Debug.Log("Eka kaappi tos!");
+            }
+
+            if (other.gameObject.name == "tokaKaappiWren")
+            {
+                Debug.Log("Toka kaappi tos!");
+            }
+
+            if (other.gameObject.name == "sinkWren")
+            {
+                Debug.Log("Sink tos!");
+            }
+
+            if (other.gameObject.name == "vessaWren")
+            {
+                Debug.Log("Vessa tos!");
+            }
+
+            if (other.gameObject.name == "boxesWren")
+            {
+                Debug.Log("Laatikoit tos!");
+            }
+
+            if (other.gameObject.name == "boxesWren2")
+            {
+                Debug.Log("Lis‰‰ laatikoit tos!");
+            }
         }
     }
 
