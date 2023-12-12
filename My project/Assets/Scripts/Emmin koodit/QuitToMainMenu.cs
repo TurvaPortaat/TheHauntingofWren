c5dbf468d065f56f76949668534a6b9faf5981f8
lattuemmi <emsv97@gmail.com>
2023-12-12 16:32:08 +0200

    Quit to Menu toimii

--- My project/Assets/Scripts/Emmin koodit/QuitToMainMenu.cs
+++ My project/Assets/Scripts/Emmin koodit/QuitToMainMenu.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitToMainMenu : MonoBehaviour
 {
-    public void 
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
     
 }
